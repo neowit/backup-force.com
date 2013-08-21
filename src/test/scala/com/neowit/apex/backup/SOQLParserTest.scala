a952c60a781549ef907176354a118624425f0d81
A. G <catch+github@neowit.com>
2013-08-21 19:35:39 +0100

    update "lastQueryProps" tests

--- src/test/scala/com/neowit/apex/backup/SOQLParserTest.scala
+++ src/test/scala/com/neowit/apex/backup/SOQLParserTest.scala
@@ -1,10 +1,23 @@
 package com.neowit.apex.backup
 
 
-import org.scalatest.{PrivateMethodTester, FunSuite}
+import org.scalatest.{FunSuite}
 import java.util.Properties
+import java.io.{File, FileWriter}
 
-class SOQLParserTest extends FunSuite with PrivateMethodTester {
+class SOQLParserTest extends FunSuite {
+
+    def withFile(testCode: (File, FileWriter) => Any) {
+        val file = File.createTempFile("test", ".properties") // create the fixture
+        val writer = new FileWriter(file)
+        try {
+            testCode(file, writer) // "loan" the fixture to the test
+        } finally {
+            // clean up the fixture
+            writer.close()
+            file.delete()
+        }
+    }
 
     test("Select From") {
         val p1 = new SOQLParser("select Id, Name from Account")
@@ -30,15 +43,28 @@ class SOQLParserTest extends FunSuite with PrivateMethodTester {
     }
 
     test("LastModifiedDate Replacement") {
-        val lastQueryProps = PrivateMethod[Properties]('lastQueryProps)
-        val props = Config invokePrivate lastQueryProps()
-        props.setProperty("account", "1917-01-01T00:00:00Z")
-        val soql1 = "select Id, name from Account where LastModifiedDate >= $Object.Lastmodifieddate"
-        assert("where LastModifiedDate >= 1917-01-01T00:00:00Z" == new SOQLParser(soql1).tail)
 
-        val soql2 = "select Id, name from Account where LastModifieddate >= $Object.Lastmodifieddate and F__c > 0 and createdDate <= $object.lastModifiedDate "
-        assert("where LastModifieddate >= 1917-01-01T00:00:00Z and F__c > 0 and createdDate <= 1917-01-01T00:00:00Z" == new SOQLParser(soql2).tail)
+        //Config is a singleton, so have to cheat to force re-initialisation of Config object
+        val c = Config.getClass.getDeclaredConstructor()
+        c.setAccessible(true)
+        c.newInstance()
 
+        withFile { (file, writer) =>
+            withFile {(f2, w2) =>
+                val props2 = new Properties() with PropertiesOption
+                props2.setProperty("account", "1917-01-01T00:00:00Z")
+                props2.store(w2, "")
+
+                Config.load(List("--config", file.getAbsolutePath, "--lastRunOutputFile", f2.getAbsolutePath))
+
+                val soql1 = "select Id, name from Account where LastModifiedDate >= $Object.Lastmodifieddate"
+                assert("where LastModifiedDate >= 1917-01-01T00:00:00Z" == new SOQLParser(soql1).tail)
+
+                val soql2 = "select Id, name from Account where LastModifieddate >= $Object.Lastmodifieddate and F__c > 0 and createdDate <= $object.lastModifiedDate "
+                assert("where LastModifieddate >= 1917-01-01T00:00:00Z and F__c > 0 and createdDate <= 1917-01-01T00:00:00Z" == new SOQLParser(soql2).tail)
+            }
+
+        }
     }
 
     test("Query & QueryAll") {
